57a65ce298b15db9a495e3c163ef5bc9bb552452
Michael Royzen <michael@mlabtechnologies.com>
2016-03-30 14:11:07 -0700

    Formatting + validation

--- Memory/Memory.py
+++ Memory/Memory.py
@@ -2,7 +2,9 @@ import random
 class Memory:
 
     def __init__(self):
-        self.colNum = input("Welcome to Memory!  Please choose the size of your board.")
+        self.colNum = 1
+        while self.colNum % 2 != 0:
+            self.colNum = input("Welcome to Memory!  Please choose the width of your square board (must an even number).")
         self.data = [ [' ']*self.colNum for row in range(self.colNum) ]
         self.numData= []
         #self.answerData
@@ -26,7 +28,7 @@ class Memory:
         x = 0
         for i in range(W):
                 x += 1
-                s += " "
+                s += "  "
                 s += str(x)
            # the string to return
         # and the numbers underneath here
@@ -34,18 +36,25 @@ class Memory:
         return s
 
     def createBoard(self):
-        for i in range(0, self.colNum):
-            self.numData.append(i)
-            self.numData.append(i)
-        random.shuffle(self.numData)
-        H = self.colNum
-        W = self.colNum
-        i = 0
-        for row in range(0,H-1):
-            for col in range(0,W-1):
-                self.data[row][col] = self.numData[2*row+col]
+        for i in range((self.colNum * self.colNum) / 2):
+                self.numData.append(i)
+                self.numData.append(i)
+            random.shuffle(self.numData)
+            H = self.colNum
+            W = self.colNum
+            i = 0
+            for row in range(H):
+                for col in range(W):
+                    numString = str(self.numData[i])
+                    if len(numString) == 1:
+                        print len(self.data)
+                        print i
+                        self.data[row][col] = str(self.numData[i]) + " "
+                    else:
+                        self.data[row][col] = str(self.numData[i])
+                    i += 1
+            self.hostGame()
 
-        self.hostGame()
 
     def hostGame(self):
         print self
@@ -53,6 +62,7 @@ class Memory:
         inputArray = user_input.split(',')
         chosenRow = int(inputArray[0])
         chosenColumn = int(inputArray[1])
+
 
 
 
