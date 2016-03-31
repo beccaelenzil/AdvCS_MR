fcd6b38cb9ec8833fc7db7cecf31fda84adcc357
Michael Royzen <michael@mlabtechnologies.com>
2016-03-31 10:48:17 -0700

    Updated formatting + reveal  + functional hostgame

--- Memory/Memory.py
+++ Memory/Memory.py
@@ -17,7 +17,10 @@ class Memory:
         s = ""
         colCounter = 1
         for row in range(0,H):
-            s += str(colCounter) + ' |'
+            if self.colNum > 8 and row < 9:
+                s += str(colCounter) + '  |'
+            else:
+                s += str(colCounter) + ' |'
             colCounter += 1
             for col in range(0,W):
                     s += str(self.data[row][col]) + '|'
@@ -25,10 +28,12 @@ class Memory:
 
         #s += (2*W+1) * '-'    # bottom of the board
         s += '  '
-        x = 0
-        for i in range(W):
+        x = 1
+        s += "  "
+        s += str(x)
+        for i in range(W - 1):
                 x += 1
-                s += "  "
+                s += "   "
                 s += str(x)
            # the string to return
         # and the numbers underneath here
@@ -39,37 +44,41 @@ class Memory:
         for i in range((self.colNum * self.colNum) / 2):
             self.numData.append(i)
             self.numData.append(i)
-            random.shuffle(self.numData)
-            H = self.colNum
-            W = self.colNum
-            i = 0
-            for row in range(H):
-                for col in range(W):
-                    numString = str(self.numData[i])
-                    if len(numString) == 1:
-                        print len(self.data)
-                        print i
-                        #self.data[row][col] = str(self.numData[i]) + " "
-                        self.data[row][col] = "*"
-                    else:
-                        #self.data[row][col] = str(self.numData[i])
-                        self.data[row][col] = "*"
-                    i += 1
-            self.hostGame()
+        random.shuffle(self.numData)
+        H = self.colNum
+        W = self.colNum
+        i = 0
+        for row in range(H):
+            for col in range(W):
+                numString = str(self.numData[i])
+                if len(numString) == 1:
+                    #self.data[row][col] = str(self.numData[i]) + " "
+                    self.data[row][col] = " * "
+                else:
+                    #self.data[row][col] = str(self.numData[i])
+                    self.data[row][col] = " * "
+                i += 1
+        self.hostGame()
 
 
     def hostGame(self):
-        print self
-        user_input = raw_input("Please choose a space to reveal in the format 'row,column'.")
-        inputArray = user_input.split(',')
-        chosenRow = int(inputArray[0])
-        chosenColumn = int(inputArray[1])
-        self.data[chosenRow][chosenColumn] = self.numData[chosenRow][chosenColumn]
-        print self
-
-
+        isPlaying = True
+        while (isPlaying):
+            print self
+            user_input = raw_input("Please choose a space to reveal in the format 'row,column'.")
+            inputArray = user_input.split(',')
+            chosenRow = int(inputArray[0])
+            chosenColumn = int(inputArray[1])
+            numString = str(chosenColumn)
+            if len(self.data[chosenRow][chosenColumn]) == 1:
+                #self.data[row][col] = str(self.numData[i]) + " "
+                self.data[chosenRow][chosenColumn] = " " + str(self.numData[chosenRow]) + " "
+            else:
+                #self.data[row][col] = str(self.numData[i])
+                self.data[chosenRow][chosenColumn] = str(self.numData[chosenRow]) + " "
+            print self
 
 
 my = Memory()
 
-print my
+#print my
