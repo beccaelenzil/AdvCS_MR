00ce677863f4f9069c484464db4cb9e3b2a788e8
Michael Royzen <michael@mlabtechnologies.com>
2016-05-19 10:52:00 -0700

    Final project utils

--- maps/utils.py
+++ maps/utils.py
@@ -16,8 +16,7 @@ def map_and_filter(s, map_fn, filter_fn):
     [1, 9, 25]
     """
     # BEGIN Question 0
-    "*** REPLACE THIS LINE ***"
-    return ['REPLACE THIS WITH YOUR LIST COMPREHENSION']
+    return [map_fn(x) for x in s if filter_fn(x) == True]
     # END Question 0
 
 def key_of_min_value(d):
@@ -30,8 +29,7 @@ def key_of_min_value(d):
     'c'
     """
     # BEGIN Question 0
-    "*** REPLACE THIS LINE ***"
-    return min('REPLACE THIS WITH YOUR SOLUTION')
+    return min(d, key = d.get)
     # END Question 0
 
 def zip(*sequences):
@@ -56,7 +54,8 @@ def zip(*sequences):
 def enumerate(s, start=0):
     """Returns a list of lists, where the i-th list contains i+`start` and the
     i-th element of `s`.
-
+]
+'
     >>> enumerate([6, 1, 'a'])
     [[0, 6], [1, 1], [2, 'a']]
     >>> enumerate('five', 5)
@@ -64,6 +63,7 @@ def enumerate(s, start=0):
     """
     # BEGIN Question 0
     "*** REPLACE THIS LINE ***"
+    return zip(s)
     # END Question 0
 
 def distance(pos1, pos2):
